9f9f7a249bae1c1fe75d78b1b2f7fe8d80fe378c
ATHARV REVDIKAR <atharv_revdikar@condenast.com>
2021-10-24 00:17:43 +0530

    emailer

--- src/routes/test-list-routes.ts
+++ src/routes/test-list-routes.ts
@@ -8,88 +8,85 @@ const router: Router = Router();
 
 
 router.get('/getTestType', (req: Request, res: Response, next: NextFunction) => {
-  res.send({...testType})
+  res.send({ ...testType })
 })
 
 router.post('/', async (req: Request, res: Response, next: NextFunction) => {
-    const date = new Date(new Date().toISOString().split('T')[0])
-    const time = new Date().toISOString().split('T')[1].split('.')[0];
-    const { testType, userId }:any = req?.body
-    console.log(date,time,testType)
-    let testList = await TestList.find({date: {$gte: date}, testType, isConducted: false})
-    let resultsFromUserEnroll:any = await UserTest
+  const date = new Date(new Date().toISOString().split('T')[0])
+  const time = new Date().toISOString().split('T')[1].split('.')[0];
+  const { testType, userId }: any = req?.body
+  console.log(date, time, testType)
+  let testList = await TestList.find({ date: { $gte: date }, testType, isConducted: false })
+  let resultsFromUserEnroll: any = await UserTest
     .find({ userId }).populate('userId').populate('testId');
-    const newtestList = []
-    for (let i = 0; i < testList.length; i++) {
-      const element : any= testList[i];
-      const isPresent = resultsFromUserEnroll.find((x:any )=> x && x.testId && x.testId.id === element.id)
-      const newElem = {...element._doc}
-      newElem.hasEnrolled  = isPresent ? true :false
-      newtestList.push(newElem)
-    }
+  const newtestList = []
+  for (let i = 0; i < testList.length; i++) {
+    const element: any = testList[i];
+    const isPresent = resultsFromUserEnroll.find((x: any) => x && x.testId && x.testId.id === element.id)
+    const newElem = { ...element._doc }
+    newElem.hasEnrolled = isPresent ? true : false
+    newtestList.push(newElem)
+  }
 
-    return res.send({status: true, data: newtestList})
+  return res.send({ status: true, data: newtestList })
 
 });
 
 
 router.post('/testList', (req: Request, res: Response, next: NextFunction) => {
 
-    const testList: ITestModel = new TestList();
-    const {
-        date= new Date(),
-        isConducted= false,
-        isTestStarted=false,
-        testName= '',
-        testDescription= '',
-        testType=  '',
-        testTime= '',
-        timer= 20000,
-        maxPrize= 0,
-        minPrize= 0,
-        totalQuestions= 0,
-        entryFee= 0,
-        instruction=''
-    } = req?.body
-    
-    testList.date = new Date(date);
-    testList.isConducted  = isConducted
-    testList.isTestStarted = isTestStarted
-    testList.testName  = testName
-    testList.testDescription = testDescription
-    testList.testType=testType
-    testList.testTime= testTime
-    testList.maxPrize = maxPrize
-    testList.minPrize=minPrize
-    testList.totalQuestions = totalQuestions
-    testList.entryFee = entryFee
-    testList.timer = timer
-    testList.instruction = instruction;
-  
-    return testList.save()
-      .then(() => {
-        return res.json({user: testList.toAuthJSON()});
-      })
-      .catch(next);
-  
+  const testList: ITestModel = new TestList();
+  const {
+    date = new Date(),
+    isConducted = false,
+    isTestStarted = false,
+    testName = '',
+    testDescription = '',
+    testType = '',
+    testTime = '',
+    timer = 20000,
+    maxPrize = 0,
+    minPrize = 0,
+    totalQuestions = 0,
+    entryFee = 0,
+    instruction = ''
+  } = req?.body
+
+  testList.date = new Date(date);
+  testList.isConducted = isConducted
+  testList.isTestStarted = isTestStarted
+  testList.testName = testName
+  testList.testDescription = testDescription
+  testList.testType = testType
+  testList.testTime = testTime
+  testList.maxPrize = maxPrize
+  testList.minPrize = minPrize
+  testList.totalQuestions = totalQuestions
+  testList.entryFee = entryFee
+  testList.timer = timer
+  testList.instruction = instruction;
+
+  return testList.save()
+    .then(() => {
+      return res.json({ user: testList.toAuthJSON() });
+    })
+    .catch(next);
+
 });
 
 router.post('/getFreetest', async (req: Request, res: Response, next: NextFunction) => {
   const list = [{
-    "timer" : 25.0,
-    "testName" : "Free test",
-    "testDescription" : "Its a Free UPSC Test",
-    "testType" : "UPSC",
-    "totalQuestions" : 10.0,
-    "entryFee" : 0.0,
-    "instruction" : "<ul>Select the option and click the submit button to successfully submit your answer.<ul><br> 30% TDS applicable to all the prizes more ₹10,<ul><br> Prizes are subject to the user agreement and other terms &amp; conditions. Please read them carefully. <ul><br> Amount of Rewards is subject to change if the total seats are not filled before 30 minutes of the test. Users would be notified accordingly.<ul><br> *Terms &amp; Conditions apply",
-    "__v" : 0.0,}
-  ]
-
-  const questionsAndAnswer: any[] = [
-    [      {
+    "timer": 25.0,
+    "testName": "Free test",
+    "testDescription": "Its a Free UPSC Test",
+    "testType": "UPSC",
+    "totalQuestions": 10.0,
+    "entryFee": 0.0,
+    "instruction": "<ul>Select the option and click the submit button to successfully submit your answer.<ul><br> 30% TDS applicable to all the prizes more ₹10,<ul><br> Prizes are subject to the user agreement and other terms &amp; conditions. Please read them carefully. <ul><br> Amount of Rewards is subject to change if the total seats are not filled before 30 minutes of the test. Users would be notified accordingly.<ul><br> *Terms &amp; Conditions apply",
+    "__v": 0.0,
+    questionAnswer: [{
       "testName": "Free Mock Test",
-      "options": ["1  only", "2 only","Both 1 & 2","None"],
+      "options": ["1  only", "2 only", "Both 1 & 2", "None"],
       "question": "Which of the following is correct : 1- Animal fat and Resin was used in making paint 2 -Paint brushes were made out of plant fiber",
       "answer": "Both 1 & 2",
       "__v": 0
@@ -152,17 +149,19 @@ router.post('/getFreetest', async (req: Request, res: Response, next: NextFuncti
     },
     {
       "testName": "Free Mock Test",
-      "options": ["1 only","2 and 3 only", "3 only","1, 2 and 3"],
+      "options": ["1 only", "2 and 3 only", "3 only", "1, 2 and 3"],
       "question": "Consider the following statements: 1.Coal ash contains arsenic, lead and mercury. 2. Coal-fire power plants release sulphur dioxide and oxides of nitrogen into the environment. 3. High ash content is observed in Indian coal. Which of the following statements given above is/are correct?",
       "answer": "1, 2 and 3",
       "__v": 0
     }]
-    ]
+  }
+  ]
 
 
-  return res.send({list,questionsAndAnswer: questionsAndAnswer })
+
+  return res.send({ list })
 })
-  
+
 
 
 export const TestListRoutes: Router = router;
